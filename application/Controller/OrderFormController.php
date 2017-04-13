6de4232443262d15d2422ef1ba5c8a01377b5873
pieter <pieter1998@protonmail.com>
2017-04-13 10:15:30 +0200

    bootstrap installatie

---
+++ application/Controller/OrderFormController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gebruiker
+ * Date: 13-4-2017
+ * Time: 09:41
+ */
+
+namespace Mini\Controller;
+
+
+class OrderFormController
+{
+
+    public function index()
+    {
+        // load views
+        require APP . 'view/_templates/headerstart.php';
+        require APP . 'view/orderform/index.php';
+        require APP . 'view/_templates/footer.php';
+
+    }
+
+}
