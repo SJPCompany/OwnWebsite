d88bdf640a2b7dc6fe9ed2bd8286087b8dba052a
Stef Bakker <stefbakker@live.nl>
2017-04-13 11:56:03 +0200

    Update

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -8,9 +8,9 @@
     </script>
 
     <!-- our JavaScript -->
-    <script src="<?php echo URL; ?>js/application.js"></script>
-    <script src="<?php echo URL; ?>js/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo URL; ?>public/js/jquery-3.2.1.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo URL; ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo URL; ?>public/js/bootstrap.min.js"></script>
+    <script src="<?php echo URL; ?>public/js/application.js"></script>
 </body>
 </html>
