27dd5c64a8cecb436c54b0a63e14cd8dbb3efb29
Stef Bakker <stefbakker@live.nl>
2017-04-13 12:04:54 +0200

    Added the database

--- application/view/home/startpage.php
+++ application/view/home/startpage.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <title>Simple Sidebar - Start Bootstrap Template</title>
-</head>
-
 <body>
 
 <div id="wrapper">
--- sjp (1).sql
+++ sjp (1).sql
@@ -1,67 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Gegenereerd op: 06 apr 2017 om 08:20
--- Serverversie: 5.7.14
--- PHP-versie: 5.6.25
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sjp`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server versie:                5.7.14 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Versie:              9.4.0.5125
 -- --------------------------------------------------------
 
---
--- Tabelstructuur voor tabel `account`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `account` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL DEFAULT '0',
-  `password` varchar(1024) NOT NULL DEFAULT '0',
-  `role` varchar(50) NOT NULL DEFAULT '0',
-  `email` varchar(50) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Gegevens worden geëxporteerd voor tabel `account`
---
-
+-- Dumpen data van tabel sjp.account: ~0 rows (ongeveer)
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
 INSERT INTO `account` (`id`, `username`, `password`, `role`, `email`) VALUES
-(1, 'juju125', '$2y$10$j.666Bssis1YND9DIsfR8u25jzgXXtv5l1Snc8cypJqlJwlBG59mq', 'member', 'justin555@live.nl');
+	(1, 'juju125', '$2y$10$j.666Bssis1YND9DIsfR8u25jzgXXtv5l1Snc8cypJqlJwlBG59mq', 'member', 'justin555@live.nl'),
+	(2, 'Administrator', 'ihebzinineenbroodje', 'admin', 'admin@admin.nl');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
 
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
