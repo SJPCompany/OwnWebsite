7c939e8bd29321f8fae09ca1763e7dde57a0118a
Stef Bakker <stefbakker@live.nl>
2017-04-18 15:21:01 +0200

    Added an contact button into the menu.

--- application/Controller/ContactController.php
+++ application/Controller/ContactController.php
@@ -16,5 +16,6 @@ class ContactController
         require APP . 'view/_templates/header.php';
         require APP . 'view/contact/index.php';
         require APP . 'view/_templates/footer.php';
+        require APP . 'view/_templates/side_menu.php';
     }
 }
