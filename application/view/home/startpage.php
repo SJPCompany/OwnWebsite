8c10c5ace2e396c1d4a287bb156f69df57a28131
Justin van de laar <justin555@live.nl>
2017-04-18 14:25:13 +0200

    changed bit of format code

--- application/view/home/startpage.php
+++ application/view/home/startpage.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-    	<div class="">
-			<p> 
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dignissimos soluta ipsum quos quia cumque sed temporibus, eaque reiciendis, nesciunt sit ab vero, repellendus voluptates animi. Exercitationem, alias, 	quidem! Nulla. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem in animi cumque, inventore 	asperiores omnis tempore nemo facere provident quam! Modi quae porro consequatur omnis illum facere iste nulla 	aspernatur!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dignissimos soluta ipsum quos 	quia cumque sed temporibus, eaque reiciendis, nesciunt sit ab vero, repellendus voluptates animi. 			Exercitationem, alias, quidem! Nulla.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium 		dignissimos soluta ipsum quos quia cumque sed temporibus, eaque reiciendis, nesciunt sit ab vero, repellendus 	voluptates animi. Exercitationem, alias, quidem! Nulla.Lorem ipsum dolor sit amet, consectetur adipisicing 		elit. Praesentium dignissimos soluta ipsum quos quia cumque sed temporibus, eaque reiciendis, nesciunt sit ab	vero, repellendus voluptates animi. Exercitationem, alias, quidem! Nulla.
-			</p>		
-		</div>
-
-
-
-
-
-
-
-
-
-
-</body>
-</html>
+<div class="">
+    <p>
+        Welcome to the scansystem
+    </p>
+</div>
