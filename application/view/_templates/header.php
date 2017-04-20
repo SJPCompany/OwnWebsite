ff588531a473e2cd23cc6dc737beceba7f4de7eb
Stef Bakker <stefbakker@live.nl>
2017-04-20 14:27:34 +0200

    Added a favicon and added info about sjp when hover over copyright.

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -5,6 +5,7 @@
     <title>Scansystem</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='shortcut icon' type='image/x-icon' href='<?php echo URL; ?>img/favicon.ico' />
 
     <!-- JS -->
     <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
