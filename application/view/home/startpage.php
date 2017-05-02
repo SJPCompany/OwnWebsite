24f71c65cecb9d307aef7b00a483dfb049c2a99c
Stef Bakker <stefbakker@live.nl>
2017-05-02 15:00:53 +0200

    Commit

--- application/view/home/startpage.php
+++ application/view/home/startpage.php
@@ -1,4 +1,22 @@
 <div class="main-text">
 	<h1 id="front-text">Welcome to the scansystem</h1>
-
+</div>
+<div class="changes">
+    <table style="width:100%">
+        <tr>
+            <th>Date</th>
+            <th>Version</th>
+            <th>Changes</th>
+        </tr>
+        <tr>
+            <td>2-5-2017</td>
+            <td>1.2</td>
+            <td>Added changes table</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 </div>
