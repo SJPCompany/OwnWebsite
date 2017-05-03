c87ff5f9e8f2f23b01667deb33b2e011cdd8a217
Stef Bakker <stefbakker@live.nl>
2017-05-03 12:57:39 +0200

    no message

--- application/view/_templates/side_menu.php
+++ application/view/_templates/side_menu.php
@@ -1,39 +1,39 @@
 <div id="sidebar">
 
     <div class="fa fa-home fa-2x" style="color: #0f0f0f;">
-        <span>Homepage</span>
+        <a href="<?php echo URL; ?>home/startpage">Homepage</a>
     </div>
 
     <div class="fa fa-search fa-2x" style="color: #0f0f0f;">
-        <span>Search-part</span>
+        <a href="<?php echo URL; ?>search/index">Search</a>
     </div>
 
     <div class="fa fa-shopping-basket fa-2x" style="color: #0f0f0f;">
-        <span>Order-Formulier</span>
+        <a href="<?php echo URL; ?>orderform/index">Order&#160;Formulier</a>
     </div>
 
     <div class="fa fa-plus fa-2x" style="color: #0f0f0f;">
-        <span>Add-part</span>
+        <a href="<?php echo URL; ?>add/add">Add&#160;part</a>
     </div>
 
     <div class="fa fa-usd fa-2x" style="color: #0f0f0f;">
-        <span>Price-Checker</span>
+        <a href="#">Price&#160;Checker</a>
     </div>
 
     <div class="fa fa-user-o fa-2x" style="color: #0f0f0f;">
-        <span>Add-User</span>
+        <a href="#">Add&#160;User</a>
     </div>
 
     <div class="fa fa-user-circle fa-2x" style="color: #0f0f0f;">
-        <span>Current-User:</span>
+        <a href="#">Current&#160;User:</a>
     </div>
 
     <div class="fa fa-question fa-2x" style="color: #0f0f0f;">
-        <span>Contact/Problems</span>
+        <a href="<?php echo URL; ?>contact/index">Contact/Problems</a>
     </div>
 
     <div class="fa fa-power-off fa-2x" style="color: #0f0f0f;">
-        <span>Logout</span>
+        <a href="#">Logout</a>
     </div>
 
 
