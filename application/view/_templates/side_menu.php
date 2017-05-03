7ff170bbda08e6d5719616eed93e0d9187994677
Justin van de laar <justin555@live.nl>
2017-05-03 12:57:56 +0200

    Dump

--- application/view/_templates/side_menu.php
+++ application/view/_templates/side_menu.php
@@ -1,38 +1,38 @@
 <div id="sidebar">
 
-    <div class="fa fa-home fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-home fa-2x rightie" style="color: #0f0f0f;">
         <span>Homepage</span>
     </div>
 
-    <div class="fa fa-search fa-2x" style="color: #0f0f0f;">
-        <span>Search-part</span>
+    <div class="fa fa-search fa-2x rightie" style="color: #0f0f0f;">
+        <span>Search part</span>
     </div>
 
-    <div class="fa fa-shopping-basket fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-shopping-basket fa-2x rightie" style="color: #0f0f0f;">
         <span>Order-Formulier</span>
     </div>
 
-    <div class="fa fa-plus fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-plus fa-2x rightie" style="color: #0f0f0f;">
         <span>Add-part</span>
     </div>
 
-    <div class="fa fa-usd fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-usd fa-2x rightie" style="color: #0f0f0f;">
         <span>Price-Checker</span>
     </div>
 
-    <div class="fa fa-user-o fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-user-o fa-2x rightie" style="color: #0f0f0f;">
         <span>Add-User</span>
     </div>
 
-    <div class="fa fa-user-circle fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-user-circle fa-2x rightie" style="color: #0f0f0f;">
         <span>Current-User:</span>
     </div>
 
-    <div class="fa fa-question fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-question fa-2x rightie" style="color: #0f0f0f;">
         <span>Contact/Problems</span>
     </div>
 
-    <div class="fa fa-power-off fa-2x" style="color: #0f0f0f;">
+    <div class="fa fa-power-off fa-2x rightie" style="color: #0f0f0f;">
         <span>Logout</span>
     </div>
 
