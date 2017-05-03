59455e518344d4f5a49dafa66f88a526c516e08c
Justin van de laar <justin555@live.nl>
2017-05-03 13:26:40 +0200

    Added links and remove duplicates

--- application/view/_templates/side_menu.php
+++ application/view/_templates/side_menu.php
@@ -1,60 +1,39 @@
 <div id="sidebar">
     <ul>
-        <li>
-            <div class="fa fa-home fa-2x" style="color: #0f0f0f;">
-                <a href="<?php echo URL; ?>home/startpage">Homepage</a>
-            </div>
-        </li>
-            <div class="fa fa-search fa-2x" style="color: #0f0f0f;">
-                <a href="<?php echo URL; ?>search/index">Search</a>
-            </div>
-            <div class="fa fa-home fa-2x" style="color: #0f0f0f;">
-                <a href="<?php echo URL; ?>home/startpage">Homepage</a>
-            </div>
-            <div class="fa fa-shopping-basket fa-2x" style="color: #0f0f0f;">
-                <a href="<?php echo URL; ?>orderform/index">Order&#160;Formulier</a>
-            </div>
+        <div class="fa fa-home fa-2x" style="color: #0f0f0f;">
+            <a href="<?php echo URL; ?>home/startpage">Homepage</a>
+        </div>
+        <div class="fa fa-search fa-2x" style="color: #0f0f0f;">
+            <a href="<?php echo URL; ?>search/index">Search</a>
+        </div>
+        <div class="fa fa-shopping-basket fa-2x" style="color: #0f0f0f;">
+            <a href="<?php echo URL; ?>orderform/index">Order&#160;Formulier</a>
+        </div>
         <div class="fa fa-plus fa-2x" style="color: #0f0f0f;">
-            <a href="<?php echo URL; ?>add/add">Add&#160;part</a>
+            <a href="#">Add&#160;part</a>
         </div>
 
         <div class="fa fa-usd fa-2x" style="color: #0f0f0f;">
             <a href="#">Price&#160;Checker</a>
         </div>
 
-    <div class="fa fa-plus fa-2x" style="color: #0f0f0f;">
-        <a href="<?php echo URL; ?>#">Add&#160;part</a>
-    </div>
-
         <div class="fa fa-user-o fa-2x" style="color: #0f0f0f;">
-            <a href="#">Add&#160;User</a>
-        </div>
-
-        <div class="fa fa-user-circle fa-2x" style="color: #0f0f0f;">
-            <a href="#">Current&#160;User:</a>
+            <a href="<?php echo URL; ?>admin/adduser">Add&#160;User</a>
         </div>
 
         <div class="fa fa-question fa-2x" style="color: #0f0f0f;">
             <a href="<?php echo URL; ?>contact/index">Contact/Problems</a>
         </div>
 
-    <div class="fa fa-user-o fa-2x" style="color: #0f0f0f;">
-        <a href="<?php echo URL; ?>admin/adduser">Add&#160;User</a>
-    </div>
-
-    <div class="fa fa-user-circle fa-2x" style="color: #0f0f0f;">
-        <a href="#">CurrentUser: <?php if (isset($_SESSION['username'])) { echo $_SESSION['username']; }?></a>
-    </div>
-
-        <div class="fa fa-power-off fa-2x" style="color: #0f0f0f;">
-            <a href="#">Logout</a>
+        <div class="fa fa-user-circle fa-2x" style="color: #0f0f0f;">
+            <a href="#">CurrentUser: <?php if (isset($_SESSION['username'])) {
+                    echo $_SESSION['username'];
+                } ?></a>
         </div>
 
+        <div class="fa fa-power-off fa-2x" style="color: #0f0f0f;">
+            <a href="<?php echo URL; ?>home/logoutUser">Logout</a>
+        </div>
 
     </ul>
-
-
-    <div class="fa fa-power-off fa-2x" style="color: #0f0f0f;">
-        <a href="<?php echo URL; ?>home/logoutUser">Logout</a>
-    </div>
 </div>
