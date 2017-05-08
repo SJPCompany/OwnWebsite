2a1f24bedcd5556821bfda332a4d5d7ad23eceb5
Justin van de laar <justin555@live.nl>
2017-05-08 10:24:38 +0200

    changed error screen to display nice message

--- application/view/error/index.php
+++ application/view/error/index.php
@@ -2,5 +2,5 @@
 	<p>This page doens`t exist <br>
      Click below here to go back to the homepage</p> <br>
     <label>GO back to home page</label>
-    <a href="<?php echo URL; ?>home/index">Go back</a>
+    <a class="back" href="<?php echo URL; ?>home/startpage">Go back</a>
 	</div>
