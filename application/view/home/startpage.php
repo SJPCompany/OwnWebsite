5cc941f7bee7c887944bce3180b9e304211a92e4
Stef Bakker <stefbakker@live.nl>
2017-05-09 13:46:00 +0200

    Boi

--- application/view/home/startpage.php
+++ application/view/home/startpage.php
@@ -4,16 +4,17 @@
     <div class="main-text">
         <h1>Welcome to the scansystem!</h1>
     </div>
+    <br>
     <table style="width:80%;">
         <tr>
+            <th>Release</th>
             <th>Version</th>
-            <th>Changes</th>
+            <th>Released Changes</th>
         </tr>
         <tr>
+            <td>8-5-2017</td>
             <td>3.5</td>
-            <td>Completely changed menu, it is responsive now.<br>
-                Added a table to insert new parts.<br>
-            </td>
+            <td>Completely changed menu, it is responsive now.<br></td>
         </tr>
         <tr>
             <td></td>
